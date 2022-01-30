6417e50e8aa1f0c23b0fd76a4e5aa848d28f6bcb
rikiapst <rikiastewart.rs@gmail.com>
2022-01-29 19:32:37 -0600

    Sending data from client to server

--- index.js
+++ index.js
@@ -2,3 +2,15 @@ const express = require('express');
 const app = express();
 app.listen(4000, () => console.log('Listening at Port 4000'));
 app.use(express.static('public'))
+app.use(express.json({ limit: '1mb' }));
+
+app.post('/api', (request, response) => {
+  console.log('Request Recieved')
+  console.log(request.body);
+  const data = request.body;
+  response.json({
+    status: 'success',
+    latitude: data.lat,
+    longitude: data.lon
+  })
+})
