e82963f55c63b12cc7567787ce781ccceb0ba04d
rikiapst <rikiastewart.rs@gmail.com>
2022-01-30 00:15:41 -0600

    Connecting and sending data to external database

--- index.js
+++ index.js
@@ -3,18 +3,25 @@ const app = express();
 app.listen(4000, () => console.log('Listening at Port 4000'));
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }));
+const { Pool } = require('pg');
 
-const database = [];
+const pool = new Pool();
 
-app.post('/api', (request, response) => {
-  console.log('Request Recieved')
-  console.log(request.body);
-  const data = request.body;
-  database.push(data)
-  console.log(database)
-  response.json({
-    status: 'success',
-    latitude: data.lat,
-    longitude: data.lon
+async function tester() {
+  app.post('/api', (request, response) => {
+    // console.log('Request Recieved')
+    // console.log(request.body);
+    const data = request.body;
+    // database.push(data)
+    // console.log(database)
+    response.json({
+      status: 'success',
+      latitude: data.lat,
+      longitude: data.lon
+    })
+    pool.query(`INSERT INTO location(longitude, latitude) VALUES('${data.lon}', '${data.lat}')`);
   })
-})
+
+}
+
+tester();
