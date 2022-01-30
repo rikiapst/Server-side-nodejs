3ea9b2c6663a5e1cadf2cd8a5c4168b678a5d76d
rikiapst <rikiastewart.rs@gmail.com>
2022-01-29 21:38:56 -0600

    NPM PG install

--- index.js
+++ index.js
@@ -4,10 +4,14 @@ app.listen(4000, () => console.log('Listening at Port 4000'));
 app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }));
 
+const database = [];
+
 app.post('/api', (request, response) => {
   console.log('Request Recieved')
   console.log(request.body);
   const data = request.body;
+  database.push(data)
+  console.log(database)
   response.json({
     status: 'success',
     latitude: data.lat,
