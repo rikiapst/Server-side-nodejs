8689675663b641a075a4df72999a8c318e45ddbe
rikiapst <rikiastewart.rs@gmail.com>
2022-02-02 00:30:53 -0600

    Weather API

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
+require('dotenv').config()
+const axios = require('axios').default;
 const express = require('express');
 const app = express();
-app.listen(4000, () => console.log('Listening at Port 4000'));
-app.use(express.static('public'))
 app.use(express.json({ limit: '1mb' }));
-const { Pool } = require('pg');
 
-const pool = new Pool();
 
-async function tester() {
+async function location() {
   app.post('/api', (request, response) => {
-    // console.log('Request Recieved')
-    // console.log(request.body);
     const data = request.body;
-    // database.push(data)
-    // console.log(database)
     response.json({
       status: 'success',
       latitude: data.lat,
       longitude: data.lon
     })
-    pool.query(`INSERT INTO location(longitude, latitude) VALUES('${data.lon}', '${data.lat}')`);
   })
 
 }
 
-tester();
+
+location();
+
+app.get(
+  '/weather', async (req, res) => {
+    var options = {
+      method: 'GET',
+      url: 'https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly',
+      params: {
+        lat: req.query.lat.toString(), lon: req.query.lon.toString()
+      },
+      headers: {
+        'x-rapidapi-host': 'weatherbit-v1-mashape.p.rapidapi.com',
+        'x-rapidapi-key': 'd9fb8c1bb7msh404a91baddfafd2p1991fcjsn15f07bb75d03'
+      }
+    };
+    let result = await axios.get('https://weatherbit-v1-mashape.p.rapidapi.com/forecast/3hourly', options);
+    let item = result.data
+
+    res.send(
+      {
+        city_name: item.city_name,
+      }
+    )
+  }
+)
+
+
+app.listen(4000, () => console.log('Listening at Port 4000'));
+app.use(express.static('public'))
